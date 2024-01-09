663160ba283d3919171c3ccd3208024aad7f6f8a
Lithnell <starwolf334@gmail.com>
2024-01-09 13:56:18 -0700

    added save + open

--- 24newproj/24newproj/Form1.vb
+++ 24newproj/24newproj/Form1.vb
@@ -1,16 +1,25 @@
-﻿Public Class Form1
+﻿Imports System.IO
+
+Public Class Form1
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
     End Sub
-
     Private Sub ExitToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ExitToolStripMenuItem.Click
         Me.Close()
     End Sub
     Private Sub NewToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles NewToolStripMenuItem.Click
         TextBox1.Clear()
     End Sub
-
-    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+    Private Sub SaveToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SaveToolStripMenuItem.Click
+        Dim outfile As New IO.StreamWriter("out.txt")
+        outfile.Write(TextBox1.Text)
+        outfile.Close()
 
     End Sub
-
+    Private Sub OpenToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles OpenToolStripMenuItem.Click
+        If File.Exists("out.txt") Then
+            Dim infile As New IO.StreamReader("out.txt")
+            TextBox1.Text = infile.ReadToEnd
+            infile.Close()
+        End If
+    End Sub
 End Class
